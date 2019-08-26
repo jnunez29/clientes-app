4f7c8ef4636d2f77d3e113af00a958c81f686e2c
jnunez29 <jorgenunezsolis@gmail.com>
2019-08-26 07:43:34 -0500

    1233

---
+++ src/app/facturas/models/factura.ts
@@ -0,0 +1,11 @@
+import { ItemFactura } from './item-factura';
+import { Cliente } from '../../clientes/cliente';
+export class Factura {
+    id: number;
+    descripcion: string;
+    observacion: string;
+    items: Array<ItemFactura> = [];
+    cliente: Cliente;
+    total: number;
+    createaT: string;
+}
